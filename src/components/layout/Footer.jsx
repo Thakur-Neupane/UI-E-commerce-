e7a4e22504ebf693bc4ba60e3a1b8cf2619b1fc0
Thakur Neupane <thakur.neupane.neupane@gmail.com>
2024-06-28 20:22:55 +1000

    footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Footer = () => {
   return (
     <div className="bg-dark text-light p-3 text-center">
-      Admin CMS for ABC org. Admin v3.6
+      Admin cms for RS gifting store
     </div>
   );
 };
